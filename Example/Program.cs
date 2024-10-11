049a949480a1a291cabb0d9a81ffc36dffe89e92
Denis Ostapovich <vofksden@gmail.com>
2024-10-11 19:06:17 +0300

    Added example project

---
+++ Example/Program.cs
@@ -0,0 +1,10 @@
+﻿using ShapeLibrary.Shapes;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var circle = new Circle(1);
+        var triangle = new Triangle(2, 3, 4);
+    }
+}
