a29dc9ef25d675c9d54eaccf8470c9d97a6ceaee
Denis Ostapovich <vofksden@gmail.com>
2024-10-13 20:49:14 +0300

    Added tests for Circle class

--- ShapeLibrary.Tests/ShapeTests/CircleTests.cs
+++ ShapeLibrary.Tests/ShapeTests/CircleTests.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using FluentAssertions;
 using ShapeLibrary.Shapes;
 
@@ -10,6 +11,36 @@ public class CircleTests
     {
         var act = () => new Circle(1);
 
+
         act.Should().NotThrow();
+    }
+
+    [Fact]
+    public void Circle_Circle_ThrowsInvalidArgumentExceptionOnZeroAndNegativeValues()
+    {
+        var act = () => new Circle(0);
+
+        act.Should().Throw<InvalidEnumArgumentException>();
+    }
+
+    [Fact]
+    public void Circle_Radius_ReturnsCorrectRadius()
+    {
+        double expectedRadius = 1;
+        var circle = new Circle(expectedRadius);
+
+        circle.Radius.Should().Be(expectedRadius);
+    }
+
+    [Fact]
+    public void Circle_CalculateArea_ReturnsCorrectArea()
+    {
+        double radius = 3;
+        double expectedArea = Math.PI * radius * radius;
+        var circle = new Circle(radius);
+
+        double resultArea = circle.CalculateArea();
+
+        resultArea.Should().Be(expectedArea);
     }
 }
