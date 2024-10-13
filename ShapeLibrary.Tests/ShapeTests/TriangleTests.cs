6624d1bd1694e3717db41a02d3e5d3d49106f98d
Denis Ostapovich <vofksden@gmail.com>
2024-10-13 22:27:53 +0300

    Refactor: changed Fact to Theory in Triangle class tests

--- ShapeLibrary.Tests/ShapeTests/TriangleTests.cs
+++ ShapeLibrary.Tests/ShapeTests/TriangleTests.cs
@@ -7,61 +7,86 @@ namespace ShapeLibrary.Tests.ShapeTests;
 
 public class TriangleTests
 {
-    [Fact]
-    public void Triangle_Triangle_CreatesTriangleInstance()
+    static readonly double hyp = double.Hypot(3, 3);
+
+    [Theory]
+    [InlineData(1, 2.5, 3)]
+    [InlineData(2, 2, 2)]
+    [InlineData(5, 7, 9)]
+    public void Triangle_Triangle_CreatesTriangleInstance(double a, double b, double c)
     {
-        var act = () => new Triangle(4, 7, 9);
+        var act = () => new Triangle(a, b, c);
 
         act.Should().NotThrow();
     }
 
-    [Fact]
-    public void Triangle_Triangle_ThrowsInvalidArgumentExceptionOnZeroOrNegativeValues()
+    [Theory]
+    [InlineData(-5, 7, 9)]
+    [InlineData(5, -7, 9)]
+    [InlineData(5, 7, -9)]
+    [InlineData(5, -7, -9)]
+    [InlineData(-5, -7, -9)]
+    [InlineData(-5, -7, 9)]
+    [InlineData(-5, 7, -9)]
+    [InlineData(0, -7, 9)]
+    [InlineData(5, 0, 9)]
+    [InlineData(5, 7, 0)]
+    [InlineData(0, 0, 0)]
+    public void Triangle_Triangle_ThrowsInvalidArgumentExceptionOnZeroOrNegativeValues(double a, double b, double c)
     {
-        var act = () => new Triangle(-1, 7, 9);
+        var act = () => new Triangle(a, b, c);
 
         act.Should().Throw<ArgumentException>();
     }
 
-    [Fact]
-    public void Triangle_Triangle_ThrowsInvalidArgumentExceptionOnInvalidTriangleSides()
+    [Theory]
+    [InlineData(1, 1, 3)]
+    [InlineData(3, 1, 1)]
+    [InlineData(1, 3, 1)]
+    public void Triangle_Triangle_ThrowsInvalidArgumentExceptionOnInvalidTriangleSides(double a, double b, double c)
     {
-        var act = () => new Triangle(1, 1, 3);
+        var act = () => new Triangle(a, b, c);
 
         act.Should().Throw<ArgumentException>();
     }
 
-    [Fact]
-    public void Triangle_A_ReturnsCorrectSideA()
+    [Theory]
+    [InlineData(1, 3, 3)]
+    [InlineData(0.2, 0.3, 0.4)]
+    [InlineData(10000, 12, 10001)]
+    public void Triangle_A_ReturnsCorrectSideA(double a, double b, double c)
     {
-        double expectedA = 1;
-        var triangle = new Triangle(expectedA, 5, 4.5);
+        var triangle = new Triangle(a, b, c);
 
-        double resultA = triangle.A;
+        double result = triangle.A;
 
-        resultA.Should().Be(expectedA);
+        result.Should().Be(a);
     }
 
-    [Fact]
-    public void Triangle_B_ReturnsCorrectSideB()
+    [Theory]
+    [InlineData(1, 3, 3)]
+    [InlineData(0.2, 0.3, 0.4)]
+    [InlineData(10000, 12, 10001)]
+    public void Triangle_B_ReturnsCorrectSideB(double a, double b, double c)
     {
-        double expectedB = 5;
-        var triangle = new Triangle(1, expectedB, 4.5);
+        var triangle = new Triangle(a, b, c);
 
-        double resultB = triangle.B;
+        double result = triangle.B;
 
-        resultB.Should().Be(expectedB);
+        result.Should().Be(b);
     }
 
-    [Fact]
-    public void Triangle_C_ReturnsCorrectSideC()
+    [Theory]
+    [InlineData(1, 3, 3)]
+    [InlineData(0.2, 0.3, 0.4)]
+    [InlineData(10000, 12, 10001)]
+    public void Triangle_C_ReturnsCorrectSideC(double a, double b, double c)
     {
-        double expectedC = 4.5;
-        var triangle = new Triangle(1, 5, expectedC);
+        var triangle = new Triangle(a, b, c);
 
-        double resultC = triangle.C;
+        double result = triangle.C;
 
-        resultC.Should().Be(expectedC);
+        result.Should().Be(c);
     }
 
     [Fact]
@@ -75,53 +100,57 @@ public class TriangleTests
         resultEpsilon.Should().Be(expectedEpsilon);
     }
 
-    [Fact]
-    public void Triangle_SetEpsilon_SetsCorrectValue()
+    [Theory]
+    [InlineData(0)]
+    [InlineData(100)]
+    [InlineData(1e-6)]
+    public void Triangle_SetEpsilon_SetsCorrectValue(double expected)
     {
-        double expectedEpsilon = 0;
         var triangle = new Triangle(7, 4, 9);
 
-        triangle.Epsilon = expectedEpsilon;
+        triangle.Epsilon = expected;
         double resultEpsilon = triangle.Epsilon;
 
-        resultEpsilon.Should().Be(expectedEpsilon);
+        resultEpsilon.Should().Be(expected);
     }
 
-    [Fact]
-    public void Triangle_CalculateArea_ReturnsCorrectArea()
+    [Theory]
+    [InlineData(5, 7, 9)]
+    [InlineData(0.2, 0.3, 0.4)]
+    [InlineData(15000, 30000, 40000)]
+    public void Triangle_CalculateArea_ReturnsCorrectArea(double a, double b, double c)
     {
-        double sideA = 4;
-        double sideB = 7;
-        double sideC = 9;
-        double simePerimeter = (sideA + sideB + sideC) / 2;
-        double expectedArea = Math.Sqrt(simePerimeter * (simePerimeter - sideA) * (simePerimeter - sideB) * (simePerimeter - sideC));
-        var triangle = new Triangle(sideA, sideB, sideC);
+        double simePerimeter = (a + b + c) / 2;
+        double expectedArea = Math.Sqrt(simePerimeter * (simePerimeter - a) * (simePerimeter - b) * (simePerimeter - c));
+        var triangle = new Triangle(a, b, c);
 
         double resultArea = triangle.CalculateArea();
 
         resultArea.Should().Be(expectedArea);
     }
 
-    [Fact]
-    public void Triangle_IsRight_ReturnsTrueOnRightTriangle()
+    [Theory]
+    [InlineData(5, 7)]
+    [InlineData(0.222, 0.33333)]
+    [InlineData(2243256246.3, 1012423142.1)]
+    public void Triangle_IsRight_ReturnsTrueOnRightTriangle(double a, double b)
     {
-        double sideA = 3;
-        double sideB = 5;
-        double sideC = Math.Sqrt(sideA * sideA + sideB * sideB);
-        var triangle = new Triangle(sideA, sideB, sideC);
+        double c = Math.Sqrt(a * a + b * b);
+        var triangle = new Triangle(a, b, c);
 
         bool isRight = triangle.IsRight();
 
         isRight.Should().BeTrue();
     }
 
-    [Fact]
-    public void Triangle_IsRight_ReturnsFalseOnRegularTriangle()
+    [Theory]
+    [InlineData(5, 7)]
+    [InlineData(0.222, 0.33333)]
+    [InlineData(2243256246.3, 1012423142.1)]
+    public void Triangle_IsRight_ReturnsFalseOnRegularTriangle(double a, double b)
     {
-        double sideA = 3;
-        double sideB = 5;
-        double sideC = 7;
-        var triangle = new Triangle(sideA, sideB, sideC);
+        double c = a + b - 0.1 * (a + b);
+        var triangle = new Triangle(a, b, c);
 
         bool isRight = triangle.IsRight();
 
