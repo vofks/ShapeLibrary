d6bb3a327ee964ba891456b56be046ca36f44110
Denis Ostapovich <vofksden@gmail.com>
2024-10-13 22:36:25 +0300

    Added a usage example

--- Example/Program.cs
+++ Example/Program.cs
@@ -4,7 +4,14 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var circle = new Circle(1);
-        var triangle = new Triangle(2, 3, 4);
+        IShape circle = new Circle(1);
+        IShape triangle = new Triangle(4, 3, 2);
+        IShape rightTriangle = new Triangle(Math.Sqrt(0.4 * 0.4 + 0.3 * 0.3), 0.4, 0.3);
+
+        Console.WriteLine($"Circle area: {circle.CalculateArea()}");
+        Console.WriteLine($"Triangle area: {triangle.CalculateArea()}");
+        Console.WriteLine($"Is right triangle: {(triangle as Triangle).IsRight()}");
+        Console.WriteLine($"Triangle area: {rightTriangle.CalculateArea()}");
+        Console.WriteLine($"Is right triangle: {(rightTriangle as Triangle).IsRight()}");
     }
 }
