a95f40284ffbede133b7b661566c5bba666f335f
Denis Ostapovich <vofksden@gmail.com>
2024-10-13 21:43:58 +0300

    Implemented Triangle class

--- ShapeLibrary/Triangle.cs
+++ ShapeLibrary/Triangle.cs
@@ -1,13 +1,28 @@
+using System.ComponentModel;
+
 namespace ShapeLibrary.Shapes;
 
 public class Triangle : IShape
 {
+    public double Epsilon { get; set; } = 1e-10;
     public double A { get; }
     public double B { get; }
     public double C { get; }
 
     public Triangle(double a, double b, double c)
     {
+        if (a <= 0 || b <= 0 || c <= 0)
+        {
+            throw new ArgumentException("Sides must be greater than zero.", a <= 0 ? nameof(a) : b <= 0 ? nameof(b) : nameof(c));
+        }
+
+        if (a + b <= c ||
+            a + c <= b ||
+            b + c <= a)
+        {
+            throw new ArgumentException("Sides do not form a triangle.");
+        }
+
         A = a;
         B = b;
         C = c;
@@ -15,13 +30,20 @@ public class Triangle : IShape
 
     public double CalculateArea()
     {
-        var semi = (A + B + C) / 2;
+        double semiPerimeter = (A + B + C) / 2;
 
-        return Math.Sqrt(semi * (semi - A) * (semi - B) * (semi - C));
+        return Math.Sqrt(semiPerimeter * (semiPerimeter - A) * (semiPerimeter - B) * (semiPerimeter - C));
     }
 
     public bool IsRight()
     {
-        throw new NotImplementedException();
+        double[] sides = [A, B, C];
+        Array.Sort(sides);
+
+        double sideA = sides[0];
+        double sideB = sides[1];
+        double hypotenuse = sides[2];
+
+        return Math.Abs(hypotenuse * hypotenuse - (sideA * sideA + sideB * sideB)) <= Epsilon;
     }
 }
